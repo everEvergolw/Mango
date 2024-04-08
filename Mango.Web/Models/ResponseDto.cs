ec7a820f06fb3d6efae2e6bf924d7f21305f6887
10925 <10925@MINE>
2024-04-09 00:23:04 +0100

    Update Toastr

---
+++ Mango.Web/Models/ResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Models
+{
+    public class ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool isSuccess { get; set; } = true; 
+
+        public string Message { get; set; }
+
+
+
+    }
+}
---
+++ Mango.Web/Service/IService/IBaseService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+
+
+namespace Mango.Web.Service.IService
+{
+    public interface IBaseService
+    {
+       Task<ResponseDto?>  SendAsync(RequestDto requestDto);
+
+
+
+
+
+    }
+}
